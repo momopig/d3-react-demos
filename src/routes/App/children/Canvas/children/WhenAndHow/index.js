c6a7bf6f5e32c9d52fcaea158b089f729991a7e3
Jamter <121978614@qq.com>
2020-02-19 00:42:08 +0800

    feat(d3-canvas): add demo links(WORKING WITH D3.JS AND CANVAS: WHEN AND HOW)

--- src/routes/App/children/Canvas/children/WhenAndHow/index.js
+++ src/routes/App/children/Canvas/children/WhenAndHow/index.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import NoDataBind from './children/NoDataBind'
 import DataBind from './children/DataBind'
 import DataBindTransition from './children/DataBindTransition'
 
 const WhenAndHow = (props) => {
   const { match } = props
+  const BASE_URL = '/canvas/whenAndHow'
   return (
-    <Switch>
-      <Route path={`${match.url}/noDataBind`} component={NoDataBind}/>
-      <Route path={`${match.url}/dataBind`} component={DataBind}/>
-      <Route path={[`${match.url}/dataBindTransition`, `${match.url}/`]} component={DataBindTransition}/>
-    </Switch>
+    <div>
+      <p><Link to={`${BASE_URL}/NoDataBind`}>1. NoDataBind</Link></p>
+      <p><Link to={`${BASE_URL}/DataBind`}>2. DataBind</Link></p>
+      <p><Link to={`${BASE_URL}/DataBindTransition`}>3. DataBindTransition</Link></p>
+      <Switch>
+        <Route path={`${match.url}/noDataBind`} component={NoDataBind}/>
+        <Route path={`${match.url}/dataBind`} component={DataBind}/>
+        <Route path={[`${match.url}/dataBindTransition`, `${match.url}/`]} component={DataBindTransition}/>
+      </Switch>
+    </div>
+
   )
 }
 export default WhenAndHow
